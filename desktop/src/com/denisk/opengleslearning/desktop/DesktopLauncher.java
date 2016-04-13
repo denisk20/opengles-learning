a26bf638b7a0cb7bdb863928fd60a9a36c3fba7f
Denis Kniazhev <dkniazhev@spredfast.com>
2016-04-13 13:00:40 +0300

    Initial commit.

---
+++ desktop/src/com/denisk/opengleslearning/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.denisk.opengleslearning.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.denisk.opengleslearning.OpenglEsLearningGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OpenglEsLearningGame(), config);
+	}
+}
