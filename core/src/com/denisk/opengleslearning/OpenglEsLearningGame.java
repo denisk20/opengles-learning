d55f9dd02bc04d7b4799561a824687d448331b22
Denis Kniazhev <denis.k1985@gmail.com>
2016-05-23 21:59:13 +0300

    Basic mesh triangle works.

--- core/src/com/denisk/opengleslearning/OpenglEsLearningGame.java
+++ core/src/com/denisk/opengleslearning/OpenglEsLearningGame.java
@@ -3,55 +3,97 @@ package com.denisk.opengleslearning;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 public class OpenglEsLearningGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	//Position attribute - (x, y)
+	public static final int POSITION_COMPONENTS = 2;
 
-	final float[] triangle1VerticesData = {
-		// X, Y, Z,
+	//Color attribute - (r, g, b, a)
+	public static final int COLOR_COMPONENTS = 4;
+
+	//Total number of components for all attributes
+	public static final int NUM_COMPONENTS = POSITION_COMPONENTS + COLOR_COMPONENTS;
+
+	//The "size" (total number of floats) for a single triangle
+	public static final int PRIMITIVE_SIZE = 3 * NUM_COMPONENTS;
+
+	//The maximum number of triangles our mesh will hold
+	public static final int MAX_TRIS = 1;
+
+	//The maximum number of vertices our mesh will hold
+	public static final int MAX_VERTS = MAX_TRIS * 3;
+
+//The array which holds all the data, interleaved like so:
+//    x, y, r, g, b, a
+//    x, y, r, g, b, a,
+//    x, y, r, g, b, a,
+
+ 	final float[] verts = {
+		// X, Y,
 		// R, G, B, A
-		-0.5f, -0.25f, 0.0f,
+		0, 0,
 		1.0f, 0.0f, 0.0f, 1.0f,
 
-		0.5f, -0.25f, 0.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
+		100, 100f,
+		1.0f, 0.0f, 0.0f, 1.0f,
 
-		0.0f, 0.559016994f, 0.0f,
-		0.0f, 1.0f, 0.0f, 1.0f};
+		100f, 0,
+		1.0f, 0.0f, 0.0f, 1.0f
+	};
 
-
-
-	/** How many bytes per float. */
-	private final int mBytesPerFloat = 4;
-	/** Store our model data in a float buffer. */
-	private FloatBuffer mTriangle1Vertices;
-	private FloatBuffer mTriangle2Vertices;
-	private FloatBuffer mTriangle3Vertices;
+	private OrthographicCamera cam;
+	private ShaderProgram shaderProgram;
+	private Mesh mesh;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		mTriangle1Vertices = ByteBuffer.allocateDirect(triangle1VerticesData.length * mBytesPerFloat)
-			.order(ByteOrder.nativeOrder()).asFloatBuffer();
-		mTriangle1Vertices.put(triangle1VerticesData).position(0);
+		mesh = new Mesh(true, MAX_VERTS, 0,
+				new VertexAttribute(VertexAttributes.Usage.Position, POSITION_COMPONENTS, "a_position"),
+				new VertexAttribute(VertexAttributes.Usage.ColorUnpacked, COLOR_COMPONENTS, "a_color"));
+		mesh.setVertices(verts);
+		Gdx.gl20.glDepthMask(false);
+		Gdx.gl.glEnable(GL20.GL_BLEND);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
+		cam = new OrthographicCamera();
+		ShaderProgram.pedantic = true;
+		shaderProgram = new ShaderProgram(
+				Gdx.files.internal("shader.vert"),
+				Gdx.files.internal("shader.frag")
+		);
+		if(! shaderProgram.isCompiled()) {
+			throw new IllegalArgumentException("Couldn't compile shaderProgram");
+		}
+		String log = shaderProgram.getLog();
+		if(log != null && log.length() > 0) {
+			System.out.println("Log: " + log);
+		}
 	}
 
+	@Override
+	public void resize(int width, int height) {
+		cam.setToOrtho(true, width, height);
+	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		shaderProgram.begin();
+		shaderProgram.setUniformMatrix("u_projTrans", cam.combined);
+		mesh.render(shaderProgram, GL20.GL_TRIANGLES, 0, verts.length/NUM_COMPONENTS);
+		shaderProgram.end();
+	}
+
+	@Override
+	public void dispose() {
+		mesh.dispose();
+		shaderProgram.dispose();
 	}
 }
